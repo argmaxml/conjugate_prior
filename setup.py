5e84b8a7165106140424c89bd098ac321210cd00
Uri Goren <uri@goren.ml>
2020-08-11 17:31:27 +0300

    Adding long_description to pypi

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     long_description=readme,
     long_description_content_type="text/markdown",
-    version='0.35',
+    version='0.36',
     description='Bayesian Statistics conjugate prior distributions',
     author='Uri Goren',
     author_email='uri@goren4u.com',
