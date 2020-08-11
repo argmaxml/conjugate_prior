90321e608f03cca987bf045f95c3f2d34d7d9832
Uri Goren <uri@goren.ml>
2020-08-11 17:56:58 +0300

    version bump up

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     long_description=readme,
     long_description_content_type="text/markdown",
-    version='0.38',
+    version='0.4',
     description='Bayesian Statistics conjugate prior distributions',
     author='Uri Goren',
     author_email='uri@goren.ml',
