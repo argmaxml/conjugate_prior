1dc4ba8405c8e3b39143ebf9b130b879aaa80367
Uri Goren <uri@goren.ml>
2020-11-07 23:59:51 +0200

    fix issue @3

--- setup.py
+++ setup.py
@@ -1,30 +1,17 @@
 from distutils.core import setup
-import re
-from pathlib import Path
-
-__name__ = 'conjugate_prior'
-__path__ = Path(__file__).parent.absolute()
-with (__path__/__name__/"__init__py").open('r') as f:
-    version = re.findall(r"__version__\s*=\s*['\"]([\d.]+)['\"]", f.read())[0]
-
-try:
-    with (__path__/"README.md").open('r') as f:
-        readme = f.read()
-except FileNotFoundError:
-    readme = ""
 
 setup(
     name=__name__,
-    packages=[__name__],
+    packages=["conjugate_prior"],
     install_requires=[
         'setuptools',
         'scipy',
         'numpy',
         'matplotlib',
     ],
-    long_description=readme,
+    long_description="https://github.com/urigoren/conjugate_prior/blob/master/README.md",
     long_description_content_type="text/markdown",
-    version=version,
+    version="0.55",
     description='Bayesian Statistics conjugate prior distributions',
     author='Uri Goren',
     author_email='uri@goren.ml',
